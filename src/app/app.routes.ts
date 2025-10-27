a3ab964a57ab40fed9cf2fa52cf5554de17f7d84
Sumita Bhowmick <SBhowmick2@slb.com>
2025-10-27 10:28:50 +0530

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AddEmployee } from './components/add-employee/add-employee';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { DataBinding } from './components/data-binding/data-binding';
+import { StructureDirective } from './directive/structure-directive/structure-directive';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'add-emp',
+        component:AddEmployee
+    },
+    {
+        path: 'emp-list',
+        component:EmployeeList
+    },
+    {
+        path: 'data-binding',
+        component:DataBinding
+    },
+    {
+        path: 'structure-Directive',
+        component:StructureDirective
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AddEmployee } from './components/add-employee/add-employee';
 import { EmployeeList } from './components/employee-list/employee-list';
 import { DataBinding } from './components/data-binding/data-binding';
@@ -7,10 +7,16 @@ import { StructureDirective } from './directive/structure-directive/structure-di
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AddEmployee, EmployeeList, StructureDirective],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  constructor(private router:Router){
+
+  }
   protected readonly title = signal('angular_18_tutorial');
+  navigateToURL(){
+    this.router.navigateByUrl("emp-list");
+  }
 }
